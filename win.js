2017fcb76bb8bdb5e28b8bac5eb64da849adb557
shriyans jindal <shriyansjindal40517@gmail.com>
2023-11-24 21:14:48 +0530

    done

--- win.js
+++ win.js
@@ -1,7 +1,6 @@
 // getting playscore from localStorage
 let player1score = localStorage.getItem("Player1Score")
 let player2score = localStorage.getItem("Player2Score")
-
 let name = document.getElementById("name")
 // selecting score box
 const play1 = document.getElementById("play1")
@@ -14,8 +13,9 @@ let quit = document.getElementById("quit")
 // random phrases for winner
 let win =["Good game, good fight!","I can't wait to play again!","Thanks for the challenge!","I'm the ultimate champion!","I'm the master of the game!","I'm loving this game!"]
 let random = Math.floor(Math.random()*win.length)
-
 // comparing player score
+play1.style.boxShadow= 'none'
+play1.style.boxShadow= 'none'
 if(player1score>player2score){
     name.textContent = localStorage.getItem("Player1")
     document.getElementById("result").textContent = win[random] 
@@ -43,11 +43,6 @@ document.getElementById("play-again").onclick =()=>{
 
 // score board
 
-const player1 = document.getElementById("player1")
-const player2 = document.getElementById("player2")
-
-player1.innerText = localStorage.getItem("Player1")
-player2.innerText = localStorage.getItem("Player2")
 document.getElementById("player1_score").innerText = player1score
 document.getElementById("player2_score").innerText = player2score
 
